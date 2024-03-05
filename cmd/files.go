5b940c4198223bb038bcb2d4306bda2304a9d032
rajeevranjan17 <rajeev.ranjan@devtron.ai>
2024-03-05 18:21:17 +0530

    configure the metatags

--- cmd/files.go
+++ cmd/files.go
@@ -1,9 +1,10 @@
 package cmd
 
 import (
-	"fmt"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var filesCmd = &cobra.Command{
@@ -11,11 +12,18 @@ var filesCmd = &cobra.Command{
 	Short: "Show the largest files in the given path",
 	Long:  `Quickly Scan a directory anmd find large files`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("files called")
+		// fmt.Println("files called")
+		if Debug {
+			for key, value := range viper.GetViper().AllSettings() {
+				log.WithFields(log.Fields{
+					key: value,
+				}).Info("Command Flag")
+			}
+
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(filesCmd)
-	
 }
--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.18.2
 )
